eef6e64c9b1c68f65e7cd51cf8f7aed6ab5dfc00
elias <elias.jm.garcia@gmail.com>
2019-10-31 16:02:14 -0500

    updated index.js to demonstrate the changes compile and run as expected for the default board value generated in a new Manager object

--- nmm/lib/index.js
+++ nmm/lib/index.js
@@ -1,3 +1,9 @@
 var addon = require('../native');
+const { Manager } = addon;
 
-console.log(addon.hello());
+const man = new Manager();
+console.log(Manager);
+console.log(man);
+var board = man.get_board();
+console.log(board);
+console.log("Board at G7: " + board["G7"]);
